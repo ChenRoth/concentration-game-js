471c00b9651dc00ada7382578376acef0672f1b0
Chen Roth <rothchen@gmail.com>
2020-05-01 09:35:44 +0300

    add card flip logic

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 import './Card.css';
 
+const CARD_BACK_IMAGE = '/card-back.png';
+
 export class Card extends Component {
     render() {
-        const { index, image, onFlip, isFlipped } = this.props;
-        const imageUrl = isFlipped ? image : '/card-back.png';
+        const { image, isFlipped } = this.props;
+        const imageUrl = isFlipped ? image : CARD_BACK_IMAGE;
         return (
-            <div className="card" style={{backgroundImage: `url(${imageUrl})`}}/>
+            <div onClick={this.handleFlip} className="card" style={{backgroundImage: `url(${imageUrl})`}}/>
         )
+    }
+
+    handleFlip = () => {
+        const {onFlip, isFlipped, index} = this.props;
+        if (isFlipped) {
+            return;
+        }
+        onFlip(index);
     }
 }
--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import { Card } from '../Card/Card'
 
-export class Game extends Component {    
+export class Game extends Component {
+    state = {
+        isCardFlipped: false
+    }
+
     render() {
+        const { isCardFlipped } = this.state;
         return (
             <div>
-                <Card image="https://picsum.photos/id/1003/1181/1772" isFlipped index={0} />
+                <Card
+                    onFlip={this.onFlip}
+                    image="https://picsum.photos/id/1003/1181/1772" isFlipped={isCardFlipped} index={0}
+                />
             </div>
         )
+    }
+
+    onFlip = (index) => {
+        this.setState({isCardFlipped: true})
     }
 }
